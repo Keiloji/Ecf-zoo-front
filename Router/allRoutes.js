3428f169e0dc2fa606b1885139ebf257293eb384
MbarkaToure <mbarkatoure04@gmail.com>
2024-04-21 17:42:16 +0200

    add page Nos Services

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,10 +3,10 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Nos services", "Nos services", "/pages/Nos services.html", "/js/Nos services.js"),
-    new Route("/Habitats", "Habitats", "/pages/Habitats.html","/js/Habitats.js"),
-    new Route("/Connexion", "Connexion", "/pages/Connexion.html","/js/Connexion.js"),
-    new Route("/Contact", "Contact", "/pages/Contact.html","/js/Contact.js"),
+    new Route("/NosServices", "Nos Services", "/pages/NosServices.html","/js/NosServices.js"),
+    new Route("/Habitats", "Habitats", "/pages/Habitats.html"),
+    new Route("/Connexion", "Connexion", "/pages/Connexion.html"),
+    new Route("/Contact", "Contact", "/pages/Contact.html"),
 
 ];
 
