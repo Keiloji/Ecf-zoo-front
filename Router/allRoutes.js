9b70f0ca421904807afa04f4dc7abe45f6da1bb3
MbarkaToure <mbarkatoure04@gmail.com>
2024-05-04 13:14:32 +0200

    ajout page habitats

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/NosServices", "Nos Services", "/pages/NosServices.html","/js/NosServices.js"),
     new Route("/Habitats", "Habitats", "/pages/Habitats.html"),
-    new Route("/Connexion", "Connexion", "/pages/Connexion.html"),
+    new Route("/singnin", "Connexion", "/pages/signin.html"),
     new Route("/Contact", "Contact", "/pages/Contact.html"),
 
 ];
