4e1d9455b95b8371537dea1a5c1da2a79bf858b4
MbarkaToure <mbarkatoure04@gmail.com>
2024-05-06 13:40:48 +0200

    add contact

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,9 +5,9 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/NosServices", "Nos Services", "/pages/NosServices.html","/js/NosServices.js"),
     new Route("/Habitats", "Habitats", "/pages/Habitats.html"),
-    new Route("/singnin", "Connexion", "/pages/signin.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
     new Route("/Contact", "Contact", "/pages/Contact.html"),
-
+    new Route("/Account", "Mon compte", "/pages/auth/account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
